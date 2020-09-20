09d1bffa3d7a96a5d0e19d157260e817b7cf1a27
Fabricio <contact@fabricioguardia.com>
2020-09-20 00:12:02 -0700

    modified register page

--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,6 @@
 import React, { Component, useState } from 'react';
 import Nav from '../components/nav'
+import axios from 'axios'
 
 function Register() {
     const [state, setState] = useState({
@@ -19,7 +20,13 @@ function Register() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.table({ name, email, password})
+        axios.post('http://localhost:3001/api/register', {
+            name, 
+            email, 
+            password
+        })
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
     }
 
     return (
@@ -27,7 +34,7 @@ function Register() {
             <Nav></Nav>
             <div className="registration">
                 <h1 className="registration-heading">Register</h1>
-                <form action="" className="registration-form" onSubmit={handleSubmit}>
+                <form action="" className="registration-form" onSubmit={handleSubmit} autoComplete="off">
                     <input onChange={handleChange('name')} type="text" className="registration-form-input" placeholder="name" autoComplete="new-name" value={name}/>
                     <input onChange={handleChange('email')} type="email" className="registration-form-input" placeholder="email" autoComplete="new-email" value={email}/>
                     <input type="password" onChange={handleChange('password')} className="registration-form-input" placeholder="password" autoComplete="new-password" value={password}/>
