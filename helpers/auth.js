3cbc802e12064467986b38a46c8fd5b78629bb55
Fabricio <contact@fabricioguardia.com>
2020-09-27 17:12:24 -0700

    created helper functions after login

---
+++ helpers/auth.js
@@ -0,0 +1,60 @@
+import cookie from 'js-cookie'
+
+// set in cookie 
+export const setCookie = (key, value) => {
+    if(process.browser){ // if window.
+        cookie.set(key, value), {
+            expires: .0001
+        }
+    }
+}
+
+// remove from cookie
+export const removeCookie = (key) => {
+    if(process.browser){
+        cookie.set(key)
+    }
+}
+
+// get from cookie such as stored token
+// useful when we need to make a request to server with auth token
+export const getCookie = key => {
+    if(process.browser){
+        return cookie.get(key)
+    }
+}
+
+// set in localstorage 
+export const setLocalStorage = (key, value) => {
+    if(process.browser){
+        localStorage.setItem(key, JSON.stringify(value))
+    }
+}
+
+// remove from localstorage
+export const removeLocalStorage = (key) => {
+    if(process.browser){
+        localStorage.removeItem(key)
+    }
+}
+
+// authenticate user by passing data to cookie and localstoreage during signin
+export const authenticate = (response, next) => {
+    setCookie('token', reponse.data.token)
+    setLocalStorage('user', resonse.data.user)
+    next()
+}
+
+// access user info from localstorage
+export const isAuth = () => {
+    if(process.brower){
+        const cookieChecked = getCookie('token')
+        if(cookieChecked){
+            if(localStorage.getItem('user')){
+                return JSON.parse(localStorage.getItem('user'))
+            } else {
+                return false
+            }
+        }
+    }
+}
