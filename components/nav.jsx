d4e32c74914e2f97ffb51b9da6e52278eb71cad0
Fabricio <contact@fabricioguardia.com>
2020-09-28 13:45:14 -0700

    Logout and conditional rendering functionality

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Link from 'next/link';
+import {isAuth, logout} from '../helpers/auth'
 
 class Nav extends Component {
     render () {
@@ -8,8 +9,38 @@ class Nav extends Component {
                 <div className="nav-logo"> &gt; hackr.io</div>
                 <ul className='nav-list'>
                     <li className="nav-list-item"><Link href="/">Home</Link></li>
-                    <li className="nav-list-item"><Link href="/login">Login</Link></li>
-                    <li className="nav-list-item"><Link href="/register">Register</Link></li>
+                    {
+                        !isAuth() && (
+                            <React.Fragment>
+                                <li className="nav-list-item"><Link href="/login">Login</Link></li>
+                                <li className="nav-list-item"><Link href="/register">Register</Link></li>
+                            </React.Fragment>
+                        )
+                    }
+                </ul>
+                <ul className="nav-list-right">
+                    <li></li>
+                    {
+                        isAuth() && isAuth().role == 'admin' && (
+                            <li className="nav-list-right-item"><Link href="/admin">Dashboard</Link></li>
+                        )
+                    }
+
+                    {
+                        isAuth() && isAuth().role == 'subscriber' && (
+                        <li className="nav-list-right-item"><Link href="/user">
+                            {isAuth().name && (<span>
+                                {isAuth().name}, Account
+                            </span>)}
+                        </Link></li>
+                        )
+                    }
+
+                    {
+                        isAuth() && (
+                            <li className="nav-list-right-item nav-list-right-item-logout" onClick={logout}>Logout</li>
+                        )
+                    }
                 </ul>
             </div>
         )
--- helpers/auth.js
+++ helpers/auth.js
@@ -1,4 +1,5 @@
 import cookie from 'js-cookie'
+import Router from 'next/router';
 
 // set in cookie 
 export const setCookie = (key, value) => {
@@ -13,7 +14,7 @@ export const setCookie = (key, value) => {
 // remove from cookie
 export const removeCookie = (key) => {
     if(process.browser){
-        cookie.set(key)
+        cookie.remove(key)
     }
 }
 
@@ -58,4 +59,10 @@ export const isAuth = () => {
             }
         }
     }
+}
+
+export const logout = () => {
+    removeCookie('token')
+    removeLocalStorage('user')
+    Router.push('/login')
 }
