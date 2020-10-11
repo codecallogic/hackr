7d7ff5a844b0ad9b420095fdfc4b4db98c46fe61
Fabricio <contact@fabricioguardia.com>
2020-10-10 17:52:55 -0700

    create dashboard page and category page

---
+++ pages/admin/category/create.js
@@ -0,0 +1,17 @@
+import {useState, useEffect} from 'react'
+import Nav from "../../../components/nav"
+import withAdmin from "../../withAdmin"
+
+function Create() {
+    const [state, setState] = useState({
+      
+    })
+    return (
+        <div>
+            <Nav></Nav>
+            Hello 
+        </div>
+  )
+}
+
+export default Create
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,11 +1,17 @@
 import Nav from '../../components/nav'
 import withAdmin from '../withAdmin'
+import Link from 'next/link'
 
 function Admin({user}) {
     return (
         <div>
             <Nav></Nav>
-            {JSON.stringify(user)}
+            <div className="dashboard-container">
+                <span className="dashboard-typography heading-1">Admin Dashboard</span>
+                <div className="dashboard-controls">
+                    <Link href="/admin/category/create">Create Category</Link>
+                </div>
+            </div>
         </div>
   )
 }
