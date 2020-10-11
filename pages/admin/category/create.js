20c74eec9b2b2046f8a14db27af3f87e3d0d31bd
Fabricio <contact@fabricioguardia.com>
2020-10-10 18:12:30 -0700

    updated category create file

--- pages/admin/category/create.js
+++ pages/admin/category/create.js
@@ -1,11 +1,32 @@
 import {useState, useEffect} from 'react'
 import Nav from "../../../components/nav"
 import withAdmin from "../../withAdmin"
+import axios from "axios"
+import API from "../../../config"
+import {showSuccessMessage, showErrorMessage} from '../../../helpers/alerts'
 
 function Create() {
     const [state, setState] = useState({
-      
+        name: '',
+        content: '',
+        error: '',
+        suscess: '',
+        formData: process.browser && new FormData(),
+        buttonText: 'Create',
+        imageUploadText: 'Upload Image',
     })
+
+    const {name, content, error, success, formData, buttonText, imageUploadText} = state
+    
+    const handleChange = (name) => (e) => {
+        setState({
+            ...state,
+            [name]: e.target.value,
+            error: '',
+            success: '',
+        })
+    }
+    
     return (
         <div>
             <Nav></Nav>
