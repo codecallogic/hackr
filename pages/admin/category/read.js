3093d8c917eba6b08c2cbbea7be82df6d2ad48ab
Fabricio <contact@fabricioguardia.com>
2020-10-24 21:03:17 -0700

    delete category functionality

--- pages/admin/category/read.js
+++ pages/admin/category/read.js
@@ -11,9 +11,11 @@ const Read = ({user, token}) => {
         categories: [],
         error: '',
         success: '',
+        modal: false,
+        slug: null
     })
 
-    const {error, success, categories} = state
+    const {error, success, categories, modal, slug} = state
 
     useEffect( () => {
         loadCategories()
@@ -21,30 +23,69 @@ const Read = ({user, token}) => {
 
     const loadCategories = async () => {
         const response = await axios.get(`${API}/categories`)
-        setState({...state, categories: response.data})
+        setState({...state, categories: response.data, modal: false})
     }
 
-    const confirmDelete = (slug) => {
-        console.log(slug)
+    const confirmDelete = (e, slug) => {
+        e.preventDefault()
+        setState({...state, [e.target.name]: !modal, slug: slug})
+    }
+
+    const handleDelete = async (e) => {
+        e.preventDefault()
+        if(e.target.name == 'delete'){
+            try {
+                const response = await axios.delete(`${API}/category/${slug}`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+                console.log(response)
+                setState({...state, modal: false})
+                loadCategories()
+            } catch (error) {
+                console.log('ERROR DELETING', error)
+            }
+        }else{
+            setState({...state, modal: false})
+        }
     }
 
     const listCategories = () => (
         categories.map( (c, i) => (
-            <Link key={c._id} href={`/links/${c.slug}`}>
-                <div className="home-category">
-                    <img src={c.image && c.image.url} className="home-category-image" alt={c.name}></img>
-                    <div className="home-category-name">{c.name}</div>
-                    <div className="home-category-controls">
-                        <Link href={`/admin/category/${c.slug}`}>
-                            <button className="home-category-controls-update dashboard-button">Update</button>
-                        </Link>
-            
-                        <button onClick={ () => confirmDelete(c.slug)}className="home-category-controls-delete dashboard-button">Delete</button>
-                        
-                    </div>
+            <div key={c._id} className="home-category">
+                <Link key={c._id} href={`/links/${c.slug}`}>
+                <img src={c.image && c.image.url} className="home-category-image" alt={c.name}></img>
+                </Link>
+                <div className="home-category-name">{c.name}</div>
+                <div className="home-category-controls">
+                    <Link href={`/admin/category/${c.slug}`}>
+                        <button className="home-category-controls-update dashboard-button">Update</button>
+                    </Link>
+        
+                    <button name='modal' onClick={ (e) => confirmDelete(e, c.slug)}className="home-category-controls-delete dashboard-button">Delete</button>   
                 </div>
-            </Link>
+            </div>
         ))
+    )
+
+    const toggle = (e) => {
+        setState({...state, [e.target.name]: !modal})
+    }
+
+    const displayModal = () => (
+        <div className="modal">
+            <div className="modal-content">
+                <button name='modal' className="modal-content-close" onClick={toggle}>&times;</button>
+                <div className="modal-content-title heading-4">
+                    Are you sure you want to delete this category?
+                </div>
+                <div className="modal-content-controls">
+                    <button className="button-confirm" name='delete' onClick={handleDelete}>Confirm</button>
+                    <button className="button-cancel" name='cancel' onClick={handleDelete}>Cancel</button>
+                </div>
+            </div>
+        </div>
     )
     
     return (
@@ -56,6 +97,7 @@ const Read = ({user, token}) => {
             <div className="home-categories">
                 {listCategories()}
             </div>
+            {modal === true && displayModal()}
         </div>
     )
 }
