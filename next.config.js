32eccf9098d6a91f7fc1d06488613050e0af4ca1
Fabricio <contact@fabricioguardia.com>
2020-11-03 13:54:57 -0800

    config update

--- next.config.js
+++ next.config.js
@@ -11,17 +11,3 @@ module.exports = withCSS({
       FB_APP_ID: 'AMC039VZDCMPQ023',
     }
 })
-
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS({
-    devIndicators: {
-      autoPrerender: false,
-    },
-    publicRuntimeConfig: {
-      APP_NAME: 'LinkNest',
-      API: '/api',
-      PRODUCTION: true,
-      DOMAIN: 'ec2-3-101-15-110.us-west-1.compute.amazonaws.com',
-      FB_APP_ID: 'AMC039VZDCMPQ023',
-    }
-})
